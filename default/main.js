5a543734bf8d6f86c1b1befa21e50989eadd23a2
ailec0623 <ailec0623@gmail.com>
2022-04-11 04:47:14 +0800

    remove auto-constructor

--- default/main.js
+++ default/main.js
@@ -1,10 +1,8 @@
 var Creeps = require('creeps');
 var Genarator = require('genarator');
-var Extension = require('structure.extension');
 
 
 module.exports.loop = function () {
-    Extension.build(Game.spawns['Spawn1'].room, Game.spawns['Spawn1'].pos);
 
     Genarator.run(Game.spawns['Spawn1']);
     Creeps.run();
